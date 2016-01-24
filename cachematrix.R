031c10291d8d6fc7c751254e5afd6423fd70f21b
ralfcosta <costalr@gmail.com>
2016-01-24 16:16:57 -0200

    Cacheable Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program is part of the assignment 2 in R Programmin Course and is
+## designed to make possible for a matrix to be cacheable and retriveable
 
-## Write a short comment describing this function
-
+## This function makes a cacheable matrix and prepare functions to manipulate it
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) m <<- solve
+    getInverse <- function() m
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function checks if the Inverse of a matrix has already being assigned
+## and if not, creates and the inverse and store it in the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
